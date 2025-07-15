5461d87af83fa7d01520a9173201109d2ebaf394
Ayush Sharma <ayushsharma36498@gmail.com>
2025-07-15 22:28:38 +0530

    added src folder

--- hosp_backend/app.js
+++ hosp_backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL, process.env.DASHBOARD_URL,],
+    origin:[process.env.FRONTEND_URL, process.env.DASHBOARD_URL,"https://hospital-management-app-28f4.vercel.app"],
     methods: ["GET","PUT", "POST", "DELETE"],
     credentials: true
 })
